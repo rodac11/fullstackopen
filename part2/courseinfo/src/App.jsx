ef72ebd6c13e8cfde75f2fd5efbddb126d8c7691
rodac11 <r.aguilera.croasdaile@gmail.com>
2026-01-01 21:24:49 -0600

    2.2: Course information step 7

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -10,11 +10,18 @@ const Course = ({course}) => {
 const Header = ({name}) => <h1>{name}</h1>
 
 const Content = ({parts}) => {
+    
+    let sumExercises = 0
+    for (let i = 0; i < parts.length; i++) {
+	sumExercises += parts[i].exercises
+    }
+    
     return (
 	<div>    
 	    {parts.map(part =>
 		<Part key={part.id} part={part}/>
 	    )}
+	    <p><strong>total of {sumExercises} exercises</strong></p>
 	</div>
     )
 }
@@ -22,29 +29,34 @@ const Content = ({parts}) => {
 const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  return <Course course={course} />
+    const course = {
+	id: 1,
+	name: 'Half Stack application development',
+	parts: [
+	    {
+		name: 'Fundamentals of React',
+		exercises: 10,
+		id: 1
+	    },
+	    {
+		name: 'Using props to pass data',
+		exercises: 7,
+		id: 2
+	    },
+	    {
+		name: 'State of a component',
+		exercises: 14,
+		id: 3
+	    },
+	    {
+		name: 'Redux',
+		exercises: 11,
+		id: 4
+	    }   
+	]
+    }
+    
+    return <Course course={course} />
 }
 
 export default App
