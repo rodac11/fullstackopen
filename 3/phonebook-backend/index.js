8a65fc3f67c67d66bebaab1b17322f9d2d4993ef
rodac11 <r.aguilera.croasdaile@gmail.com>
2026-05-25 12:39:21 -0600

    3.19*: Phonebook database, step 7

--- 3/phonebook-backend/index.js
+++ 3/phonebook-backend/index.js
@@ -85,7 +85,8 @@ app.post('/api/persons', (request, response, next) => {
     persons = persons.concat(person)
     person.save().then(savedPerson => {   
 	response.json(savedPerson)
-    })    
+    })
+	.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -137,6 +138,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
 	return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+	return response.status(400).json({ error: error.message })
     }
     next(error)
 }
